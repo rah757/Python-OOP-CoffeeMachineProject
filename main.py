4f42b712998622b5548472be28ea6c3d3a4fc1a3
Rah <arrehman757@gmail.com>
2023-11-08 17:56:51 +0530

    completed coffee machine functions

--- main.py
+++ main.py
@@ -2,29 +2,31 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
-
-# moneyMachine = MoneyMachine()
 menu = Menu()
 coffeeMaker = CoffeeMaker()
-
+moneyMachine = MoneyMachine()
 
 print("Welcome to the Coffee Machine!")
 print(f"The items available here are: {menu.get_items()}")
-item = input(f"Enter the type of drink you would like: ").lower()
-
-if item == "report":
-    coffeeMaker.report()
-elif item == "off":
-    print("Turning off machine..")
-else:
-    drink = menu.find_drink(item)
-    if drink != None:
-        print(f"You have ordered a {drink.name}")
-
-        if coffeeMaker.is_resource_sufficient() == True:
-            print(f"The {drink.name} costs {drink.cost}$")
-
+stillInMachine = True
+while stillInMachine == True:
+    item = input(f"Enter the type of drink you would like: ").lower()
+    if item == "report":
+        coffeeMaker.report()
+        moneyMachine.report()
+    elif item == "off":
+        print("Turning off machine..")
+        stillInMachine = False
     else:
-        print("Item does not exist.")
+        drink = menu.find_drink(item)
+        if drink != None:
+            print(f"You have ordered a {drink.name}")
 
+            if coffeeMaker.is_resource_sufficient(drink) == True:
+                print(f"The {drink.name} costs {drink.cost}$")
+                moneyMachine.make_payment(drink.cost)
+                coffeeMaker.make_coffee(drink)
+        else:
+            print("Item does not exist.")
+        if input("Would you like to exit machine? If so, press 'n' : ") == 'n':
+            stillInMachine = False
